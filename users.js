85bab9d27859e006cecbd40d91b01b99f8d92e60
Thayerjam <115114070+Thayerjam@users.noreply.github.com>
2022-11-28 06:37:20 -0800

    Add files via upload
    updated to include error handling :)

---
+++ users.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const {
+  models: { User },
+} = require('../db');
+
+/**
+ * All of the routes in this are mounted on /api/users
+ * For instance:
+ *
+ * router.get('/hello', () => {...})
+ *
+ * would be accessible on the browser at http://localhost:3000/api/users/hello
+ *
+ * These route tests depend on the User Sequelize Model tests. However, it is
+ * possible to pass the bulk of these tests after having properly configured
+ * the User model's name and userType fields.
+ */
+
+// Add your routes here:
+
+router.get('/unassigned', async (req, res, next) => {
+  try {
+      res.json(await User.findUnassignedStudents());
+  } catch (err) {
+      next(err)
+  }
+})
+
+router.get('/teachers', async (req, res, next) => {
+  try {
+      res.json(await User.findTeachersAndMentees());
+  } catch (err) {
+      next(err)
+  }
+})
+
+router.delete('/:index', async (req, res, next) => {
+  try {
+      const userId = req.params.index;
+      if (isNaN(userId)) {
+        res.sendStatus(400);
+      } else {
+        const userWithGivenId = await User.findById(userId);
+        if (userWithGivenId.length === 0) {
+          res.sendStatus(404);
+        } else {
+          await User.destroy({
+            where: {
+              id: userId
+            }
+          })
+          res.sendStatus(204);
+        }
+      }
+  } catch (err) {
+      next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const isUnique = await User.findByName(req.body.name)
+    if (isUnique.length > 0) {
+      res.sendStatus(409)
+    } else {
+      const newUser = await User.create({ name: req.body.name })
+      res.status(201).json(newUser)
+    }
+  } catch (err) {
+      next(err)
+  }
+})
+
+router.put('/:id', async (req, res) => {
+  try {
+    const userToUpdate = await User.findById(req.params.id);
+    if (userToUpdate.length > 0) {
+      res.status(200).json(updatedUser)
+    } else {
+      res.sendStatus(404)
+    }
+  } catch (err) {
+      next(err)
+  }
+})
+
+module.exports = router;
